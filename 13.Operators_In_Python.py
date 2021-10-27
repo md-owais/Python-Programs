27ddc54c7297e86a59aa70cd307c031bfcb18ec3
Md Owais <84002244+md-owais@users.noreply.github.com>
2021-10-27 22:48:57 +0530

    Add files via upload

---
+++ 13.Operators_In_Python.py
@@ -0,0 +1,62 @@
+# Operators in Python
+
+# Arithmetic Operators
+# Assignment Operators
+# Comparison Operators
+# Logical Operators
+# Identity Operators
+# Membereship Operators
+# Bitwise Operators
+"""
+# Arithmetic Operators
+print("5 + 6 is", 5+6)
+print("5 - 6 is", 5-6)
+print("5 * 6 is", 5*6)
+print("5 / 6 is", 5/6)
+print("5 // 6 is", 5//6)
+print("5 ** 6 is", 5**6)
+print("5 % 6 is", 5%6)
+
+
+# Assignment Operators
+print("Assignment Operators")
+x = 5
+print(x)
+x += 7
+print(x)
+
+# Comparision Operators
+i = 8
+print(i == 8)
+print(i == 5)
+print(i <= 8)
+print(i >= 8)
+print(i <= 5)
+print(i & 5)
+
+# Logical Operators
+a = True
+b = False
+
+print(a and b)
+print(a or b)
+
+# Identity Operators
+print(a is b)
+print(a is not b)
+print(5 is not 5)
+
+# Membership Operators
+list = [3,3,2,2,39,33,35,32]
+print(324 in list)
+print(324 not in list)
+"""
+
+# Bitwise Operators
+# 0 - 00
+# 1 - 01
+# 2 - 10
+# 3 - 11
+print(0 & 1)
+print(0 | 1)
+print(0 | 3)
