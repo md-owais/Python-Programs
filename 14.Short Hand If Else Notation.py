14fd8441c267e311d12ee9cdece3b4d28e9cb4c4
Md Owais <84002244+md-owais@users.noreply.github.com>
2021-10-29 20:10:39 +0530

    Add files via upload

---
+++ 14.Short Hand If Else Notation.py
@@ -0,0 +1,16 @@
+# Short Hand If Else Notation In Python
+"""
+# If we write the statement:
+if condition:
+   if-expression
+else:
+   else-expression
+
+"""
+
+
+a = int(input("Enter a\n"))
+b = int(input("Enter b\n"))
+
+# if a>b: print("A B se bada hai bhai")
+print("B A se bada hai bhai") if a<b else print("A B se bada hai bhai")
---
+++ 15.Functions & Docstrings.py
@@ -0,0 +1,20 @@
+# Functions and Docstrings in Python
+
+"""
+a = 9
+b = 8
+c = sum((a,b))  # built in Functions
+ print(c)
+ """
+def function1(a, b):
+    print("Hello you are in function 1", a+b)
+
+def function2(a, b):
+    """This is a function which will calculate average of two numbers
+    this function dosen't work for three number's"""
+    average = (a+b)/2
+#    print(average)
+    return average
+# v = function2(5, 7)
+# print(v)
+print(function2. __doc__)
